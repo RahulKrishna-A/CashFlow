3243bbb1cf05e7355e356c44ea87d3ee64253286
Rahul <rahulkrish321123@gmail.com>
2024-03-13 04:05:42 +0530

    Added config file to store keys

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = require("./routes/index")
 const app = express()
 const cors = require("cors")
+const port = 3000
 app.use(cors())
 app.use(express.json())
 app.use("/api/v1",router)
 
+app.listen(port)
