78fcda64246996a397b79632f11aa36e13ea3b72
Rahul <rahulkrish321123@gmail.com>
2024-03-16 17:42:32 +0530

    Added Transfer funds functionality without failure protection

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -16,6 +16,37 @@ const {Account} = require("../Mongodb")
 })
 
 router.post("/transfer",authMiddleware,async(req,res)=>{
+    const transferFrom = req.userId
+    const  transferTo= req.body.to
+    const transferAmount = req.body.amount
+
+    const customerFrom = await  Account.findOne({userId:transferFrom})
+    const balanceFrom = customerFrom.balance
+
+    if(balanceFrom<transferAmount){
+        return res.status(400).json({
+            message:
+            "Insufficient Funds"
+        })
+    }
+
+    const customerTo = await Account.findOne({userId:transferTo})
+    if(!customerTo){
+        return res.status(400).json({message:"Invalid account"})
+    }
+    await Account.updateOne({userID:transferFrom},{$inc:{
+        balance : -transferAmount
+        }})
+
+    await Account.updateOne({userID:transferTo},{$inc:{
+            balance : transferAmount
+        }})
+
+    return res.status(200).json("Transfer successful")
+
+
+
+
 
 })
 
