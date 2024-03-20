b9e334fde380397a9c585a4cb05d5b194d6a55c9
Rahul <rahulkrish321123@gmail.com>
2024-03-20 16:59:12 +0530

    Added protected routes for SendMoney and Dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,19 @@
 import { Appbar } from "../components/Appbar"
 import { Balance } from "../components/Balance"
 import { Users } from "../components/Users"
+import {useNavigate} from "react-router-dom";
+import {useEffect} from "react";
 
 export const Dashboard = () => {
+
+    const navigate = useNavigate();
+    useEffect(() => {
+        if(!localStorage.getItem("token")){
+            navigate("/signin")
+        }
+    }, []);
+
+
     return <div>
         <Appbar />
         <div className="m-8">
--- frontend/src/pages/SendMoney.jsx
+++ frontend/src/pages/SendMoney.jsx
@@ -1,12 +1,20 @@
-import { useSearchParams } from 'react-router-dom';
+import {useNavigate, useSearchParams} from 'react-router-dom';
 import axios from "axios";
-import { useState } from 'react';
+import {useEffect, useState} from 'react';
 
 export const SendMoney = () => {
     const [searchParams] = useSearchParams();
     const id = searchParams.get("id");
     const name = searchParams.get("name");
     const [amount, setAmount] = useState(0);
+
+
+    const navigate = useNavigate();
+    useEffect(() => {
+        if(!localStorage.getItem("token")){
+            navigate("/signin")
+        }
+    }, []);
 
     return <div className="flex justify-center h-screen bg-gray-100">
         <div className="h-full flex flex-col justify-center">
