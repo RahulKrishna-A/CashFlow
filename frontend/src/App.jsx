359063badad9708831cb80f52015653506b380ce
Rahul <rahulkrish321123@gmail.com>
2024-03-20 16:31:59 +0530

    added basic frontend structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,25 @@
 import {
-    BrowserRouter,
-    Router,
-Route}
-import React from "react";
-
-
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import { Dashboard } from "./pages/Dashboard";
+import { SendMoney } from "./pages/SendMoney";
 
 function App() {
-
   return (
-    <div>
+    <>
        <BrowserRouter>
-           <Router>
-               <Route path={"/"} element = {}/>
-               <Route path={"/signup"} element={}/>
-               <Route path={"/signin"} element={}/>
-               <Route path={"/dashboard"} element={}/>
-               <Route path={"/send"} element={}/>
-
-
-
-           </Router>
-
-       </BrowserRouter>
-    </div>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
