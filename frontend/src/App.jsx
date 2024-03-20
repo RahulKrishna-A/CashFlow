d04eaa326f3fd9b28356c73e97b6018a1a07c912
Rahul <rahulkrish321123@gmail.com>
2024-03-20 17:33:25 +0530

    Implemented lazy loading for signup and signin pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,26 @@
+import React, {Suspense} from "react";
+
 import {
   BrowserRouter,
   Route,
   Routes,
 } from "react-router-dom";
-import { Signup } from "./pages/Signup";
-import { Signin } from "./pages/Signin";
+// import { Signup } from "./pages/Signup";
+// import { Signin } from "./pages/Signin";
 import { Dashboard } from "./pages/Dashboard";
 import { SendMoney } from "./pages/SendMoney";
+import Loading from "./pages/Loading.jsx";
+
+const Signin  = React.lazy(()=>import("./pages/Signin"))
+const Signup  = React.lazy(()=>import("./pages/Signup"))
 
 function App() {
   return (
     <>
        <BrowserRouter>
         <Routes>
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
+            <Route path="/signup" element={<Suspense fallback={<Loading/>}>  <Signup /></Suspense>} />
+            <Route path="/signin" element={<Suspense fallback={<Loading/>}> <Signin /></Suspense>} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/send" element={<SendMoney />} />
         </Routes>
--- frontend/src/pages/Loading.jsx
+++ frontend/src/pages/Loading.jsx
@@ -1,17 +1,20 @@
 export default function Loading(){
-    return<div>
-
-        <section className="bg-blue-900 relative place-items-center grid h-screen w-screen gap-4">
-
-            <div className="bg-blue-500 w-48 h-48  absolute animate-ping rounded-full delay-5s shadow-xl"></div>
-
-            <div className="bg-blue-400 w-32 h-32 absolute animate-ping rounded-full shadow-xl"></div>
-
-            <div className="bg-white w-24 h-24 absolute animate-pulse rounded-full shadow-xl"></div>
-
-            <svg xmlns="http://www.w3.org/2000/svg" className="text-blue-900 filter mix-blend-overlay h-16 w-16" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M5.636 18.364a9 9 0 010-12.728m12.728 0a9 9 0 010 12.728m-9.9-2.829a5 5 0 010-7.07m7.072 0a5 5 0 010 7.07M13 12a1 1 0 11-2 0 1 1 0 012 0z" />
-            </svg>
-        </section>
+    return(
+<div className={"h-screen w-screen flex justify-center items-center"}>
+    <div className="flex flex-col items-center">
+        <div className="text-5xl font-semibold mb-3">
+            Please wait
+        </div>
+        <div className="opacity-50 mb-8">
+            Your Page is Loading
+        </div>
+        <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle className="opacity-25" stroke="currentColor" strokeWidth="4" cx="12" cy="12" r="10"></circle>
+            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        </svg>
     </div>
-}
+</div>
+   
+
+    )
+    }
