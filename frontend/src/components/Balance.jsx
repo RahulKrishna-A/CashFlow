f4df7a8fc09e2c6421c9970e1cbfd8b22798be52
Rahul <rahulkrish321123@gmail.com>
2024-03-25 03:51:33 +0530

    Balance.jsx:Changed balance styles

--- frontend/src/components/Balance.jsx
+++ frontend/src/components/Balance.jsx
@@ -1,10 +1,10 @@
 export const Balance = ({value}) => {
-    return <div className="flex">
-        <div className="font-bold text-lg">
+    return <div className="flex ">
+        <div className="font-bold text-2xl">
             Your balance
         </div>
-        <div className="font-semibold ml-4 text-lg">
-            Rs {value}
+        <div className="font-semibold text-2xl ml-4 ">
+            $ {value}
         </div>
     </div>
 }
