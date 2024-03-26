a9c339931683d445cb8e59bdf0bad46e74a930f3
Rahul <rahulkrish321123@gmail.com>
2024-03-26 18:56:59 +0530

    Changed api calls from render to vercel

--- frontend/src/apicalls.js
+++ frontend/src/apicalls.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const BASE_URL = 'http://localhost:3000'
-const BASE_URL = 'https://cashflow-pn2p.onrender.com'
+const BASE_URL = 'https://cash-flow-five.vercel.app'
 
 
 export const signin = async (username, password) => {
